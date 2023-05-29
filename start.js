e99ccfdd9ff8b366b80235a255983e4f05ac1fda
David <77265282+nikkeijin@users.noreply.github.com>
2023-05-29 10:47:29 +0900

    Update start.js

--- start.js
+++ start.js
@@ -1,8 +1,14 @@
 import { runPuppeteer } from './src/runPuppeteer.js';
 
-// You can run one or multiple accounts at once
-runPuppeteer('first-user', 'first-password');
-runPuppeteer('second-user', 'second-password');
-runPuppeteer('third-user', 'third-password');
+function runWithDelay(user, password, delay){
+    setTimeout(() => {
+        runPuppeteer(user, password);
+    }, delay)
+}
+
+runWithDelay('first-user', 'first-password', 0);
+runWithDelay('second-user', 'second-password', 5000);
+runWithDelay('third-user', 'third-password', 10000);
+
 
 console.log('Puppeteer is scraping...');
