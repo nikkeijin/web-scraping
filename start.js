822e6aae0321167d982198a4f04aaa6f86435043
David <77265282+nikkeijin@users.noreply.github.com>
2023-05-29 11:48:24 +0900

    Update start.js

--- start.js
+++ start.js
@@ -6,9 +6,23 @@ function runWithDelay(user, password, delay){
     }, delay)
 }
 
+/* ##########
+
+The parameters user and password are optional. 
+Use the parameters if you need a user and password to log-in the website
+
+########## */
+
+runWithDelay('first-user', 'first-password', 0);
+
+/* ##########
+
+You can add multiple user if needed
+
 runWithDelay('first-user', 'first-password', 0);
 runWithDelay('second-user', 'second-password', 5000);
 runWithDelay('third-user', 'third-password', 10000);
 
+########## */
 
 console.log('Puppeteer is scraping...');
