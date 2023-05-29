c6f998587b5c5818a43ec55949ad7865bbd74cfa
David <77265282+nikkeijin@users.noreply.github.com>
2023-05-29 11:49:48 +0900

    Update runPuppeteer.js

--- src/runPuppeteer.js
+++ src/runPuppeteer.js
@@ -2,18 +2,29 @@ import puppeteer from 'puppeteer';
 import { sendLineMessage } from '../services/sendLineMessage.js';
 import { updateGoogleSheet } from '../services/updateGoogleSheet.js';
 
-//user and password parameters from start.js
+/* ########## 
+
+Example of a website with user and password. User and password parameters are from start.js
+You can erase the runPuppeteer() function parameters if you don't need to log-in.
+
+########## */
 export const runPuppeteer = async (user, password) => {
 
-  // ########## Open Browser
-  // Change headless value to true if you don't want to see the browser running
+  /* ########## 
+
+  Open the browser.
+  Change headless value to true if you don't want to see the browser running.
+
+  ########## */
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://example.com/');
   await page.setViewport({ width: 1080, height: 1024 });
+
+  /* ########## 
   
-  // ########## Loggging into the site
-  // Add your own code, this is an example code
+  Example of how to log in a website:
+
   await page.waitForTimeout(2000);
   const email = await page.$('#email');
   await email.type(user);
@@ -25,36 +36,32 @@ export const runPuppeteer = async (user, password) => {
   await Promise.all([
     page.waitForNavigation({ waitUntil: 'networkidle0' }),
     page.keyboard.press('Enter')
-  ]);
 
-  // ########## Scraping Data from the page
-  // Add your own code, this is an example code
-  const overview = await page.evaluate(() => {
+  ]); 
+  
+  ########## */
 
-    // Scraping data
-    const type = document.querySelectorAll('.overview');
-    const view = type[0].innerText;
-    const comment = type[1].innerText;
-    const like = type[2].innerText;
+  // Example of scraping data
+  const example = await page.evaluate(() => {
 
-    const date = document.querySelector('.overview-date').innerText;
+    const title = document.querySelector('h1').innerText;
+    const content = document.querySelector('p').innerText;
+    const url = document.querySelector('a').href;
 
     // This is how I send the value from the const to the sendLineMessage & updateGoogleSheet parameters
     return {
-      view: view,
-      comment: comment,
-      like: like,
-      date: date
+      title: title,
+      content: content,
+      url: url
     };
 
   });
 
-  // ########## Send Data to LINE & Google Sheet
-  // This parameter is an example, change to your own parameter
-  await sendLineMessage(overview);
-  await updateGoogleSheet(overview);
+  // Send Data to LINE & Google Sheet
+  await sendLineMessage(example);
+  await updateGoogleSheet(example);
 
-  // ########## Close Connection
+  // Close the browser
   await browser.close();
 
 };
